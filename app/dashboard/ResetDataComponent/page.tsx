4dd78dd50f164a007e12f069ff552e2a8cf4fa5b
Hassan Osama <hassasenbbbh@gmail.com>
2026-01-19 20:36:00 +0200

    fix: bypass config types and add use client

--- app/dashboard/ResetDataComponent/page.tsx
+++ app/dashboard/ResetDataComponent/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { useState } from 'react';
 import { DailyTotal, WeeklyTotal, MonthlyTotal } from "../page";
 type ResetDataProps = {
--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // هذا سيسمح بالرفع حتى لو وجد أخطاء في الـ Lint
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // هذا سيسمح بالرفع حتى لو وجد أخطاء في الـ Types
     ignoreBuildErrors: true,
   },
 };
